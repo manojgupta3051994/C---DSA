ae8e64275077d159451ad4c4f90ddc79a4853898
manoj <manojgupta3051994@gmail.com>
2022-06-12 12:53:07 +0530

    Loops Changes

--- Loops.cpp
+++ Loops.cpp
@@ -41,4 +41,49 @@ int main()
     cout << endl;
     cout << sum << endl;
 
+    // For Loop
+
+    int sum = 0;
+    int n ;
+    cin >> n;
+    for (int i = 1; i <=n ; i++){
+        sum += i;
+    }
+    cout << sum << endl;
+
+    // Fibonacci Number
+
+    int n;
+    cin >> n;
+    int a = 0;
+    int b = 1;
+
+    for (int i = 1; i <=n ; i++){
+        int fibnumber = a+b;
+        cout << fibnumber << " ";
+
+        a = b;
+        b = fibnumber;
+  }
+
+  // Prime Number 
+
+    int n;
+    cin >> n;
+    bool IsPrime = 1;
+    for (int i = 2; i <n ; i ++){
+        if (n%i==0){
+        IsPrime = 0;
+        break;
+        continue;
+        cout << "Manoj" << endl;
+        }
+    }
+    if (IsPrime == 0){
+        cout << "Not a Prime Number " << endl;
+    }
+    else{
+        cout << "Is a Prime Number " << endl;
+    }
+
 }
